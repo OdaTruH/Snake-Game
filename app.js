76b09b51f826b4bcd72c41f95a7987125424821c
Antony Hurtado <antony.hurtado58@gmail.com>
2022-02-05 16:40:45 +0100

    html+css+snakeCreation

---
+++ app.js
@@ -0,0 +1,48 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = "white";
+ctx.strokeStyle = "black";
+ctx.fillRect(0,0,canvas.widht, canvas.height);
+ctx.strokeRect(0,0,canvas.widht, canvas.height);
+
+let snake = [ {x:140, y:150}, {x:130, y:150}, {x:120, y:150}, {x:110, y:150} ]
+
+function dessineLesMorceaux(morceau) {
+
+    ctx.fillStyle = "#00fe14";
+    ctx.strokeStyle = 'black';
+    ctx.fillRect(morceau.x, morceau.y, 10, 10);
+    ctx.strokeRect(morceau.x, morceau.y, 10, 10);
+
+
+
+
+}
+// Variables
+
+// Vitesse sur X
+vx = 10;
+// Vitesse sur Y
+vy = -10;
+
+
+
+function dessineLeSerpent(){
+    snake.forEach(morceau => {
+        dessineLesMorceaux(morceau);
+    })
+}
+
+
+function faireAvancerSerpent() {
+
+    const head = {x: snake[0].x + vx, y: snake[0].y};
+    snake.unshift(head);
+    snake.pop();
+
+}
+
+faireAvancerSerpent();
+
+dessineLeSerpent();
